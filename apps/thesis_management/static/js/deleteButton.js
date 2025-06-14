b29252d6d1a4ed64eeb47c1421ce2784c697f1e6
Jhonat2709 <jhonatanddiaz7@gmail.com>
2025-06-13 22:55:27 -0400

    Ya casi

--- apps/thesis_management/static/js/deleteButton.js
+++
@@ -1,9 +0,0 @@
-export const deleteAutorRow = (event) => {
-  if (event.target && event.target.classList.contains("remove-btn")) {
-    const rowId = event.target.getAttribute("data-row-id");
-    const rowToRemove = document.getElementById(`autor-row-${rowId}`);
-    if (rowToRemove) {
-      rowToRemove.remove();
-    }
-  }
-};
